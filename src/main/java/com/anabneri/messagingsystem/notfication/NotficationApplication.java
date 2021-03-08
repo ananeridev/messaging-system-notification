2e398b5b546f9a2099a5de5f9aa0967d4d2faa6b
anabneri <you@example.com>
2021-03-08 20:04:45 -0300

    initial commit

---
+++ src/main/java/com/anabneri/messagingsystem/notfication/NotficationApplication.java
@@ -0,0 +1,13 @@
+package com.anabneri.messagingsystem.notfication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NotficationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NotficationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/anabneri/messagingsystem/notfication/NotficationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.anabneri.messagingsystem.notfication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NotficationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
