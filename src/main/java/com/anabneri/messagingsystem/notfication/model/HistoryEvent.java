92fe078ec88e83e85831411d701b05fd371e7888
anabneri <you@example.com>
2021-03-08 21:08:05 -0300

    feat(model): creating the classes for modelating the database and service

--- src/main/java/com/anabneri/messagingsystem/notfication/model/HistoryEvent.java
+++ src/main/java/com/anabneri/messagingsystem/notfication/model/HistoryEvent.java
@@ -1,4 +1,104 @@
 package com.anabneri.messagingsystem.notfication.model;
 
+import java.lang.reflect.Type;
+import java.time.LocalDateTime;
+import java.util.Objects;
+
 public class HistoryEvent {
+
+    private Integer id;
+    private Type type;
+    private Registration registrationId;
+    private LocalDateTime createdAt;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public Type getType() {
+        return type;
+    }
+
+    public void setType(Type type) {
+        this.type = type;
+    }
+
+    public Registration getSubscriptionId() {
+        return registrationId;
+    }
+
+    public void setSubscriptionId(Registration subscriptionId) {
+        this.registrationId = registrationId;
+    }
+
+    public LocalDateTime getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(LocalDateTime createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        HistoryEvent that = (HistoryEvent) o;
+        return Objects.equals(id, that.id)
+                && Objects.equals(type, that.type)
+                && Objects.equals(registrationId, that.registrationId)
+                && Objects.equals(createdAt, that.createdAt);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, type, registrationId, createdAt);
+    }
+
+    public static Builder builder() {
+        return new Builder();
+    }
+
+    public static final class Builder {
+        private Integer id;
+        private Type type;
+        private Registration registrationId;
+        private LocalDateTime createdAt;
+
+        private Builder() {
+        }
+
+        public Builder id(Integer id) {
+            this.id = id;
+            return this;
+        }
+
+        public Builder type(Type type) {
+            this.type = type;
+            return this;
+        }
+
+        public Builder subscriptionId(Registration registrationId) {
+            this.registrationId = registrationId;
+            return this;
+        }
+
+        public Builder createdAt(LocalDateTime createdAt) {
+            this.createdAt = createdAt;
+            return this;
+        }
+
+        public HistoryEvent build() {
+            HistoryEvent eventHistory = new HistoryEvent();
+            eventHistory.setId(id);
+            eventHistory.setType(type);
+            eventHistory.setSubscriptionId(registrationId);
+            eventHistory.setCreatedAt(createdAt);
+            return eventHistory;
+        }
+    }
 }
--- src/main/java/com/anabneri/messagingsystem/notfication/model/Registration.java
+++ src/main/java/com/anabneri/messagingsystem/notfication/model/Registration.java
@@ -1,4 +1,104 @@
 package com.anabneri.messagingsystem.notfication.model;
 
+import java.time.LocalDateTime;
+import java.util.Objects;
+
 public class Registration {
-}
+
+    private String id;
+    private Status status;
+    private LocalDateTime createdAt;
+    private LocalDateTime updatedAt;
+
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String activeid) {
+        this.id = id;
+    }
+
+    public Status getStatus() {
+        return status;
+    }
+
+    public void setStatus(Status status) {
+        this.status = status;
+    }
+
+    public LocalDateTime getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(LocalDateTime createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public LocalDateTime getUpdatedAt() {
+        return createdAt;
+    }
+
+    public void setUpdatedAt(LocalDateTime createdAt) {
+        this.createdAt = createdAt;
+    }
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Registration that = (Registration) o;
+        return Objects.equals(id, that.id)
+                && Objects.equals(status, that.status)
+                && Objects.equals(createdAt, that.createdAt)
+                && Objects.equals(updatedAt, that.updatedAt);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, status, createdAt, updatedAt);
+    }
+
+
+    public static Builder builder() {
+        return new Builder();
+    }
+
+    public static final class Builder {
+        private String id;
+        private Status status;
+        private LocalDateTime createdAt;
+        private LocalDateTime updatedAt;
+
+        private Builder() {
+        }
+
+        public Builder id(String id) {
+            this.id = id;
+            return this;
+        }
+
+        public Builder statusId(Status statusId) {
+            this.status = statusId;
+            return this;
+        }
+
+        public Builder createdAt(LocalDateTime createdAt) {
+            this.createdAt = createdAt;
+            return this;
+        }
+
+        public Builder updatedAt(LocalDateTime updatedAt) {
+            this.updatedAt = updatedAt;
+            return this;
+        }
+
+        public Registration build() {
+            Registration registration = new Registration();
+            registration.setId(id);
+            registration.setStatus(status);
+            registration.setCreatedAt(createdAt);
+            registration.setUpdatedAt(updatedAt);
+            return registration;
+        }
+    }
+    }
