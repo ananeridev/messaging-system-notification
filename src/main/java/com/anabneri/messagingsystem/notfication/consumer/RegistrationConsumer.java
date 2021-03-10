25f9c46b6416ce864a299e18bb5df94d76cc8f42
anabneri <you@example.com>
2021-03-10 04:13:33 -0300

    fix(error start): resolving error build

--- src/main/java/com/anabneri/messagingsystem/notfication/consumer/RegistrationConsumer.java
+++ src/main/java/com/anabneri/messagingsystem/notfication/consumer/RegistrationConsumer.java
@@ -7,24 +7,19 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.stereotype.Component;
 
 @Component
 public class RegistrationConsumer {
 
     @Autowired
-    private SimpMessagingTemplate messagingTemplate;
-
-    @Autowired
     private RegistrationService  service;
 
     private static final Logger LOGGER = LoggerFactory.getLogger(RegistrationConsumer.class);
 
-
     @RabbitListener(queues = MessagingConfig.QUEUE)
     public void consumer(RegistrationDTO registration) {
-        LOGGER.info(String.format("Consuming the subscription %s", registration.getId()));
+        LOGGER.info(String.format("Consuming the registration %s", registration.getId()));
         service.saveRegistration(registration);
     }
 
