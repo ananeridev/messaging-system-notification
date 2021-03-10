ecdbb31ef65aefb027dc30c892ad2d2eb281a888
anabneri <you@example.com>
2021-03-10 06:35:29 -0300

    feat: adding component scan

--- src/main/java/com/anabneri/messagingsystem/notfication/NotficationApplication.java
+++ src/main/java/com/anabneri/messagingsystem/notfication/NotficationApplication.java
@@ -2,8 +2,20 @@ package com.anabneri.messagingsystem.notfication;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.web.socket.config.annotation.EnableWebSocket;
+import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
 
 @SpringBootApplication
+@EntityScan("com.anabneri.messagingsystem.notfication.model")
+@EnableJpaRepositories("com.anabneri.messagingsystem.notfication.repository")
+@ComponentScan("com.anabneri.messagingsystem.notfication")
+@EnableWebSocket
+@EnableWebSocketMessageBroker
+@EnableJpaAuditing
 public class NotficationApplication {
 
 	public static void main(String[] args) {
